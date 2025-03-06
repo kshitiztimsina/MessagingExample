cf6cf102bcbcb9a68c341979b08871d8bbc548df
user <user@KSHITIZ>
2025-03-06 20:34:55 +0545

    Add project files.

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace MessagingExample.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/messages")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    private readonly ConnectionFactory _factory;
+
+    public MessageController()
+    {
+        _factory = new ConnectionFactory() { HostName = "localhost" };
+    }
+
+    [HttpPost]
+    public IActionResult SendMessage([FromBody] MessageDto message)
+    {
+        using var connection = _factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "email_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+        channel.BasicPublish(exchange: "", routingKey: "email_queue", basicProperties: null, body: body);
+
+        return Ok(new { status = "Message sent to queue" });
+    }
+}
+
+public class MessageDto
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
---
+++ Worker/ReceiverWorker.cs
@@ -0,0 +1,77 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json;
+
+namespace MessagingExample.Worker;
+public class ReceiverWorker : BackgroundService
+{
+    private readonly ILogger<ReceiverWorker> _logger;
+    private IConnection _connection;
+    private IModel _channel;
+
+    public ReceiverWorker(ILogger<ReceiverWorker> logger)
+    {
+        _logger = logger;
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.QueueDeclare(queue: "email_queue",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var emailRequest = JsonSerializer.Deserialize<MessageRequest>(message);
+
+                if (emailRequest != null)
+                {
+                    _SendEmailAsync(emailRequest);
+                }
+
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            _channel.BasicConsume(queue: "email_queue",
+                                 autoAck: false,
+                                 consumer: consumer);
+        
+
+        return Task.CompletedTask;
+    }
+
+    private void _SendEmailAsync(MessageRequest request)
+    {
+        _logger.LogInformation("{@request}", request);
+        _logger.LogInformation("Message send successfully");
+
+    }
+
+    public override void Dispose()
+    {
+        _channel?.Close();
+        _connection?.Close();
+        base.Dispose();
+    }
+}
+
+public class MessageRequest
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
+
+
+
